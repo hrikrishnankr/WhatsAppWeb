507dbd2f2235ab7e648f0b80e7e4d3188fd9c0d1
Hari Krishnan <hari@qburst.com>
2016-02-04 10:41:28 +0530

    added send message

--- JS/Views/sendTab.js
+++ JS/Views/sendTab.js
@@ -12,10 +12,11 @@ var sendTab = Backbone.View.extend({
 			message_div.render().$el.append("<i class='material-icons' id='camera_alt'>camera_alt</i>");
 
 		var bubble_pointer = addDiv(this , "bubble_pointer");
-			bubble_pointer.render().$el.html("<img src='#'/>")
 		var recordAndSend = addDiv(this , "record_and_send");
-			recordAndSend.render().$el.append("<i class='material-icons md-light'>send</i>");
-		componentHandler.upgradeDom();
+			recordAndSend.render().$el.attr("onclick",'').click(new Function("app_router.navigate('send', {trigger: true, replace: true});"));
+			recordAndSend.render().$el.append('<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect ">'+
+												'<i class="material-icons md-light">send</i></button>');
+			componentHandler.upgradeDom();
 		return this;
 	},
 });
